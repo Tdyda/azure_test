1adb0aee9235486f9c508f32be0b62e425751f35
Tomasz Dyda <tdyda@tablefoods.com>
2025-10-01 20:12:53 +0200

    api(auth): add login (with IP/UA), refresh, and logout endpoints; allow anonymous #3

--- testing/Api/Controllers/AuthController.cs
+++ testing/Api/Controllers/AuthController.cs
@@ -1,6 +1,9 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using testing.Application.Abstractions.Auth.Login;
+using testing.Application.Abstractions.Auth.Logout;
+using testing.Application.Abstractions.Auth.Refresh;
 
 namespace testing.Controllers
 {
@@ -11,11 +14,33 @@ namespace testing.Controllers
         private readonly IMediator _mediator;
         public AuthController(IMediator mediator) => _mediator = mediator;
 
+        [AllowAnonymous]
         [HttpPost("login")]
         public async Task<ActionResult<LoginResponse>> Login([FromBody] LoginRequest request, CancellationToken ct)
         {
-            var response = await _mediator.Send(new LoginCommand(request.Email, request.Password), ct);
-            return Ok(response);
+            var ip = HttpContext.Connection.RemoteIpAddress?.ToString();
+            var ua = Request.Headers.UserAgent.ToString();
+            var res = await _mediator.Send(new LoginCommand(request.Email, request.Password, ip, ua), ct);
+            return Ok(res);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("refresh")]
+        public async Task<ActionResult<RefreshTokenResponse>> Refresh([FromBody] RefreshTokenRequest req,
+            CancellationToken ct)
+        {
+            var ip = HttpContext.Connection.RemoteIpAddress?.ToString();
+            var ua = Request.Headers.UserAgent.ToString();
+            var res = await _mediator.Send(new RefreshTokenCommand(req.RefreshToken, ip, ua), ct);
+            return Ok(res);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("logout")]
+        public async Task<ActionResult> Logout([FromBody] RefreshTokenRequest req, CancellationToken ct)
+        {
+            await _mediator.Send(new RevokeRefreshTokenCommand(req.RefreshToken), ct);
+            return NoContent();
         }
     }
 }
